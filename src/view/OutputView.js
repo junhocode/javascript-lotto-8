809d669c561c62cb7873744a461d21786118dc43
진준호 <161407999+junhocode@users.noreply.github.com>
2025-10-31 18:32:47 +0900

    feat: Add additional print logics in OutputView

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -5,6 +5,23 @@ class OutputView {
   static printBoughtAmount(boughtAmount) {
     Console.print(UI_MESSGAES.BOUGHT_LOTTO_COUNT(boughtAmount));
   }
+
+  static printLottos(lottos) {
+    lottos.forEach((lotto) => {
+      const numbers = lotto.getNumbers()
+      const formattedString = `[${numbers.join(', ')}]`
+      Console.print(formattedString);
+    })
+  }
+
+  static printResultHeader() {
+    Console.print(UI_MESSGAES.RESULT_HEADER);
+  }
+  
+  static printResults(rank, roi) {
+    Console.print(`3개 일치 (5,000원) - ${rank.fifth}개\n4개 일치 (50,000원) - ${rank.fourth}개\n5개 일치 (1,500,000원) - ${rank.third}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank.second}개\n6개 일치 (2,000,000,000원) - ${rank.first}개\n총 수익률은 ${roi}%입니다.
+    `)
+  }
 }
 
 export default OutputView;
