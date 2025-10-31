988f26f853cbb762e73c8650f89146177a0e2f8c
진준호 <161407999+junhocode@users.noreply.github.com>
2025-10-31 18:28:05 +0900

    feat: Add numbers getter in Lotto

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,12 +7,17 @@ class Lotto {
   }
 
   #validate(numbers) {
+    const set = new Set(numbers)
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (numbers.length !== set.size)
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.")
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
