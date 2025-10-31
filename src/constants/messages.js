d7f7c3d948cf8c6d6379a1abd98923cef9fd4f70
진준호 <161407999+junhocode@users.noreply.github.com>
2025-10-31 18:30:17 +0900

    style: Format amount in error message with thousand separators

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,15 +1,19 @@
 import { CONSTANTS } from "./constants.js"
 
 export const UI_MESSGAES = {
-    GET_BUDGET: "구입금액을 입력해 주세요. \n",
-    BOUGHT_LOTTO_COUNT: (boughtAmount) => `${boughtAmount}개를 구매했습니다.`,
-    GET_WINNING_NUMBERS: "당첨 번호를 입력해 주세요. \n",
-    GET_BONUS_NUMBER: "보너스 번호를 입력해 주세요. \n",
-    RESULT_HEADER: "당첨 통계\n---", 
+    GET_BUDGET: "구입금액을 입력해 주세요.\n",
+    BOUGHT_LOTTO_COUNT: (boughtAmount) => `\n${boughtAmount}개를 구매했습니다.`,
+    GET_WINNING_NUMBERS: "\n당첨 번호를 입력해 주세요.\n",
+    GET_BONUS_NUMBER: "\n보너스 번호를 입력해 주세요.\n",
+    RESULT_HEADER: "\n당첨 통계\n---", 
 }
 
 export const ERROR_MESSGAES = {
     LOTTO_NOT_BOUGHT: `${CONSTANTS.ERROR_PREFIX} 로또를 구매해 주세요.`,
     INVALID_BUDGET: `${CONSTANTS.ERROR_PREFIX} 구입 금액으로 숫자 이외의 입력값을 넣을 수 없습니다.`,
-    INVALID_BUDGET_NUMBER: `${CONSTANTS.ERROR_PREFIX} 구입 금액은 ${CONSTANTS.LOTTO_PRICE}원 단위여야 합니다.`, 
+    INVALID_BUDGET_NUMBER: `${CONSTANTS.ERROR_PREFIX} 구입 금액은 ${CONSTANTS.LOTTO_PRICE.toLocaleString('ko-KR')}원 단위여야 합니다.`,
+    INVALID_WINNING_NUMBERS: `${CONSTANTS.ERROR_PREFIX} 당첨 번호로 숫자 이외의 값을 넣을 수 없습니다.`, 
+    INVALID_WINNING_NUMBERS_LENGTH: `${CONSTANTS.ERROR_PREFIX} 당첨 번호는 6개여야 합니다.`,
+    INVALID_LOTTO_NUMBER: `${CONSTANTS.ERROR_PREFIX} 로또 번호는 1과 45 사이의 값이여야 합니다.`,
+    INVALID_BONUS_NUMBER: `${CONSTANTS.ERROR_PREFIX} 보너스 숫자로 숫자 이외의 입력값을 넣을 수 없습니다.`,
 }
