5583daba74bb432028b4d9396218b89b56d66da4
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-01 22:02:01 +0900

    style: Rename printBoughtAmount

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -2,8 +2,8 @@ import { Console } from "@woowacourse/mission-utils"
 import { UI_MESSGAES } from "../constants/messages.js"
 
 class OutputView {
-  static printBoughtAmount(boughtAmount) {
-    Console.print(UI_MESSGAES.BOUGHT_LOTTO_COUNT(boughtAmount));
+  static printLottoCount(lottoCount) {
+    Console.print(UI_MESSGAES.BOUGHT_LOTTO_COUNT(lottoCount));
   }
 
   static printLottos(lottos) {
