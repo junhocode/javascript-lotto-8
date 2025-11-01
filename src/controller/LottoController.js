ec77253c2c1ada10400c796b759ec077f2fdd4a9
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-01 22:11:39 +0900

    refactor: Refactor LottoController by including Parser

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,19 +1,26 @@
 import { Console } from "@woowacourse/mission-utils";
 import InputView from "../view/InputView.js";
 import OutputView from "../view/OutputView.js";
+import Parser from "../service/models/Parser.js";
+import { retryOnException } from "../utils/retryOnException.js";
 import LottoGenerator from "../service/models/LottoGenerator.js";
 import LottoWinChecker from "../service/models/LottoWinChecker.js";
 
 class LottoController {
   async run() {
     try {
-      const amountToBuy = await this.#getValidBudget();
-      OutputView.printBoughtAmount(amountToBuy);
+      const budget = await retryOnException(InputView.getBudget());
+      const lottoCount = Parser.budgetParser(budget);
+      OutputView.printLottoCount(lottoCount);
+
+      const winningNumbersInput = await retryOnException(InputView.getWinningNumbers());
+      const winningNumbers = Parser.winningNumbersParser(winningNumbersInput);
+
+      const bonusNumberInput = await retryOnException(InputView.getBonusNumber());
+      const bonusNumber = Parser.bonusNumberParser(bonusNumberInput, winningNumbers)
+
       const lottos = LottoGenerator.generate(amountToBuy)
       OutputView.printLottos(lottos);
-
-      const winningNumbers = await this.#getValidWinningNumbers();
-      const bonusNumber = await this.#getValidBonusNumber();
 
       const winningLotto = LottoWinChecker.matchLottoNumbers(lottos, winningNumbers, bonusNumber)
       OutputView.printResultHeader();
@@ -21,43 +28,6 @@ class LottoController {
       OutputView.printResults(winningLotto, ROI)
     } catch (error) {
       throw error;
-    }
-  }
-
-  async #getValidBudget() {
-    while (true) {
-      try {
-        const budgetInput = await InputView.getLottoBudget();
-        const amountNumber = LottoGenerator.getAmountToBuy(budgetInput);
-        return amountNumber;
-      } catch (error) {
-        Console.print(error.message);
-      }
-    }
-  }
-
-  async #getValidWinningNumbers() {
-    while (true) {
-      try {
-        const winningNumbersInput = await InputView.getWinningNumbers();
-        const winningNumbers =
-          LottoWinChecker.getWinningNumbers(winningNumbersInput);
-        return winningNumbers;
-      } catch (error) {
-        Console.print(error.message);
-      }
-    }
-  }
-
-  async #getValidBonusNumber() {
-    while (true) {
-      try {
-        const bonusNumberInput = await InputView.getBonusNumber();
-        const bonusNumber = LottoWinChecker.getBonusNumber(bonusNumberInput);
-        return bonusNumber;
-      } catch (error) {
-        Console.print(error.message);
-      }
     }
   }
 }
