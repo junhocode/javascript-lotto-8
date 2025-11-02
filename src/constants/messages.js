2819bbf7aca12f021487f00619fcd8eba496f7e3
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-02 14:44:34 +0900

    reafctor: Moved Error prefix to Validation Error class

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -9,13 +9,13 @@ export const UI_MESSGAES = {
 }
 
 export const ERROR_MESSGAES = {
-    INVALID_BUDGET: `${CONSTANTS.ERROR_PREFIX} 구입 금액으로 숫자 이외의 입력값을 넣을 수 없습니다.`,
-    FALSY_BUDGET: `${CONSTANTS.ERROR_PREFIX} 구입 금액은 0원 이하가 될 수 없습니다.`,
-    INVALID_BUDGET_NUMBER: `${CONSTANTS.ERROR_PREFIX} 구입 금액은 ${CONSTANTS.LOTTO_PRICE.toLocaleString('ko-KR')}원 단위여야 합니다.`,
-    INVALID_WINNING_NUMBERS: `${CONSTANTS.ERROR_PREFIX} 당첨 번호로 숫자 이외의 값을 넣을 수 없습니다.`, 
-    INVALID_WINNING_NUMBERS_LENGTH: `${CONSTANTS.ERROR_PREFIX} 당첨 번호는 6개여야 합니다.`,
-    INVALID_LOTTO_NUMBER: `${CONSTANTS.ERROR_PREFIX} 로또의 번호는 1과 45 사이의 값이여야 합니다.`,
-    INVALID_BONUS_NUMBER: `${CONSTANTS.ERROR_PREFIX} 보너스 번호로 숫자 이외의 값을 넣을 수 없습니다.`,
-    DUPLICATE_WINNING_NUMBER: `${CONSTANTS.ERROR_PREFIX} 당첨 번호에 동일한 숫자가 포함되어 있습니다.`,
-    BONUS_NUMBER_EXISTS: `${CONSTANTS.ERROR_PREFIX} 보너스 번호는 당첨 번호와 중복될 수 없습니다.`,
+    INVALID_BUDGET: '구입 금액으로 숫자 이외의 입력값을 넣을 수 없습니다.',
+    FALSY_BUDGET: '구입 금액은 0원 이하가 될 수 없습니다.',
+    INVALID_BUDGET_NUMBER: `구입 금액은 ${CONSTANTS.LOTTO_PRICE.toLocaleString('ko-KR')}원 단위여야 합니다.`,
+    INVALID_WINNING_NUMBERS: '당첨 번호로 숫자 이외의 값을 넣을 수 없습니다.', 
+    INVALID_WINNING_NUMBERS_LENGTH: '당첨 번호는 6개여야 합니다.',
+    INVALID_LOTTO_NUMBER: '로또의 번호는 1과 45 사이의 값이여야 합니다.',
+    INVALID_BONUS_NUMBER: '보너스 번호로 숫자 이외의 값을 넣을 수 없습니다.',
+    DUPLICATE_WINNING_NUMBER: '당첨 번호에 동일한 숫자가 포함되어 있습니다.',
+    BONUS_NUMBER_EXISTS: '보너스 번호는 당첨 번호와 중복될 수 없습니다.',
 }
