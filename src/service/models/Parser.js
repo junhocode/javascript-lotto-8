126a28246b7b72f2a4989bf30bc216f13198f77d
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-02 17:45:02 +0900

    reafctor: Change Error to ValidationError

--- src/service/models/Parser.js
+++ src/service/models/Parser.js
@@ -1,41 +1,41 @@
 import { CONSTANTS } from "../../constants/constants.js"
-import { ERROR_MESSGAES } from "../../constants/messages"
+import { ERROR_MESSGAES } from "../../constants/messages.js";
+import ValidationError from "../../errors/ValidationError.js";
 
 class Parser {
     static budgetParser(budget) {
-        this.#validateBudget(budget)
-        const lottoCount = budget / CONSTANTS.LOTTO_PRICE
-        return lottoCount;
+        this.#validateBudget(Number(budget))
+        return budget;
     }
 
-    static winningNumbersParser(winningNumbers) {
-        const winningNumbersArray = Number(winningNumbers.split(','))
-        this.#validateWinningNumbers(winningNumbersArray)
-        return winningNumbersArray;
+    static winningNumbersParser(winningNumbersInput) {
+        const winningNumbers = winningNumbersInput.split(',').map(Number);
+        this.#validateWinningNumbers(winningNumbers)
+        return winningNumbers;
     }
 
-    static bonusNumberParser(bonusNumber, winningNumbersArray) {
-        this.#validateBonusNumber(bonusNumber, winningNumbersArray)
+    static bonusNumberParser(bonusNumber, winningNumbers) {
+        this.#validateBonusNumber(Number(bonusNumber), winningNumbers)
         return bonusNumber;
     }
 
     static #validateBudget(budget) {
-        if (isNaN(budget)) throw new Error(ERROR_MESSGAES.INVALID_BUDGET);
-        if (budget <= 0) throw new Error(ERROR_MESSGAES.FALSY_BUDGET);
-        if (budget % CONSTANTS.LOTTO_PRICE) throw new Error(ERROR_MESSGAES.INVALID_BUDGET_NUMBER);
+        if (isNaN(budget)) throw new ValidationError(ERROR_MESSGAES.INVALID_BUDGET);
+        if (budget <= 0) throw new ValidationError(ERROR_MESSGAES.FALSY_BUDGET);
+        if (budget % CONSTANTS.LOTTO_PRICE) throw new ValidationError(ERROR_MESSGAES.INVALID_BUDGET_NUMBER);
     }
     
-    static #validateWinningNumbers(winningNumbersArray) {
-        if (winningNumbersArray.some((number) => isNaN(number))) throw new Error(ERROR_MESSGAES.INVALID_WINNING_NUMBERS);
-        if (winningNumbersArray.length !== 6) throw new Error(ERROR_MESSGAES.INVALID_WINNING_NUMBERS_LENGTH);
-        if (winningNumbersArray.some((number) => number > CONSTANTS.MAX_LOTTO_NUMBER || number < CONSTANTS.MIN_LOTTO_NUMBER)) throw new Error(ERROR_MESSGAES.INVALID_LOTTO_NUMBER);
-        if (new Set(winningNumbersArray).size !== winningNumbersArray.length) throw new Error(ERROR_MESSGAES.DUPLICATE_WINNING_NUMBER); 
+    static #validateWinningNumbers(winningNumbers) {
+        if (winningNumbers.some((number) => isNaN(number))) throw new ValidationError(ERROR_MESSGAES.INVALID_WINNING_NUMBERS);
+        if (winningNumbers.length !== 6) throw new ValidationError(ERROR_MESSGAES.INVALID_WINNING_NUMBERS_LENGTH);
+        if (winningNumbers.some((number) => number > CONSTANTS.MAX_LOTTO_NUMBER || number < CONSTANTS.MIN_LOTTO_NUMBER)) throw new ValidationError(ERROR_MESSGAES.INVALID_LOTTO_NUMBER);
+        if (new Set(winningNumbers).size !== winningNumbers.length) throw new ValidationError(ERROR_MESSGAES.DUPLICATE_WINNING_NUMBER); 
     }
 
-    static #validateBonusNumber(bonusNumber, winningNumbersArray) {
-        if (isNaN(bonusNumber)) throw new Error(ERROR_MESSGAES.INVALID_BONUS_NUMBER);
-        if (bonusNumber > MAX_LOTTO_NUMBER || bonusNumber < CONSTANTS.MIN_LOTTO_NUMBER) throw new Error(ERROR_MESSGAES.INVALID_LOTTO_NUMBER);
-        if (winningNumbersArray.includes(bonusNumber)) throw new Error(ERROR_MESSGAES.BONUS_NUMBER_EXISTS);
+    static #validateBonusNumber(bonusNumber, winningNumbers) {
+        if (isNaN(bonusNumber)) throw new ValidationError(ERROR_MESSGAES.INVALID_BONUS_NUMBER);
+        if (bonusNumber > CONSTANTS.MAX_LOTTO_NUMBER || bonusNumber < CONSTANTS.MIN_LOTTO_NUMBER) throw new ValidationError(ERROR_MESSGAES.INVALID_LOTTO_NUMBER);
+        if (winningNumbers.includes(bonusNumber)) throw new ValidationError(ERROR_MESSGAES.BONUS_NUMBER_EXISTS);
     }
 }
 
