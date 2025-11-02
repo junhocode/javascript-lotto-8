296a1fa5b52840619f123710fd3a252590fb35bc
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-02 17:48:48 +0900

    refactor: Refactor OutputView

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,26 +1,32 @@
-import { Console } from "@woowacourse/mission-utils"
-import { UI_MESSGAES } from "../constants/messages.js"
+import { Console } from "@woowacourse/mission-utils";
+import { UI_MESSGAES } from "../constants/messages.js";
 
 class OutputView {
   static printLottoCount(lottoCount) {
-    Console.print(UI_MESSGAES.BOUGHT_LOTTO_COUNT(lottoCount));
+    Console.print(UI_MESSGAES.LOTTO_COUNT(lottoCount));
   }
 
   static printLottos(lottos) {
     lottos.forEach((lotto) => {
-      const numbers = lotto.getNumbers()
-      const formattedString = `[${numbers.join(', ')}]`
-      Console.print(formattedString);
-    })
+      const numbers = lotto.getNumbers();
+      Console.print(`[${numbers.join(", ")}]`)
+    });
   }
 
   static printResultHeader() {
     Console.print(UI_MESSGAES.RESULT_HEADER);
   }
-  
-  static printResults(rank, roi) {
-    Console.print(`3개 일치 (5,000원) - ${rank.fifth}개\n4개 일치 (50,000원) - ${rank.fourth}개\n5개 일치 (1,500,000원) - ${rank.third}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank.second}개\n6개 일치 (2,000,000,000원) - ${rank.first}개\n총 수익률은 ${roi}%입니다.
-    `)
+
+  static printResults(results) {
+    Console.print(UI_MESSGAES.RESULT(results));
+  }
+
+  static printProfitRate(profitRate) {
+    Console.print(UI_MESSGAES.PROFIT_RATE(profitRate));
+  }
+
+  static printError(error) {
+    Console.print(error.message);
   }
 }
 
